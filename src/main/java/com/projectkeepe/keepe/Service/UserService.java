23f4c470d09bdec518b6db158d36ce2d4e234cf5
a-sepulveda-r <ariel.sepulvedar@gmail.com>
2022-12-20 23:24:24 -0300

    primer commit

---
+++ src/main/java/com/projectkeepe/keepe/Service/UserService.java
@@ -0,0 +1,45 @@
+package com.projectkeepe.keepe.Service;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.projectkeepe.keepe.Model.User;
+import com.projectkeepe.keepe.Repository.UserRepository;
+
+@Service
+@Transactional
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser(User user){
+        userRepository.save(user);
+    }
+
+    public void updateUser (User user){
+        userRepository.save(user);
+    }
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public List<User> buscarPorRut(String rut){
+        return userRepository.findAllUserRut(rut);
+    }
+    public List<User> findId(Integer id){
+        return userRepository.findAllUserId(id);
+    }
+
+    public void eliminarUser (Integer id){
+        userRepository.deleteById(id);
+    }
+    
+}
